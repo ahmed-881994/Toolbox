1f0cca135caf4d6cf56e79050c15fb613eeae56a
Ahmed Safwat <ahmed-881994@hotmail.com>
2023-05-03 19:58:43 +0300

    update encryptor url

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,5 @@
 DEV_ENCRYPTOR_URL = 'http://127.0.0.1:8000/'
-ENCRYPTOR_URL ='https://encryptorapi-2-e5583038.deta.app/'
+ENCRYPTOR_URL ='https://encryptorapi-3-e5583038.deta.app/'
 
 CURRENCY_BASE_URL = 'https://api.exchangerate.host/'
 CURRENCY_CONVERT_URL=CURRENCY_BASE_URL+'convert?'
