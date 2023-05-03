42e166c66d8c23fd5ea2422f2e00966d4c81ffe1
Ahmed Safwat <ahmed-881994@hotmail.com>
2023-05-03 18:50:04 +0300

    change encryptor api url

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,5 @@
 DEV_ENCRYPTOR_URL = 'http://127.0.0.1:8000/'
-ENCRYPTOR_URL ='https://er47jlt2sy5h2o5wsr3525rgru0thllp.lambda-url.us-east-1.on.aws/'
+ENCRYPTOR_URL ='https://encryptorapi-2-e5583038.deta.app/'
 
 CURRENCY_BASE_URL = 'https://api.exchangerate.host/'
 CURRENCY_CONVERT_URL=CURRENCY_BASE_URL+'convert?'
